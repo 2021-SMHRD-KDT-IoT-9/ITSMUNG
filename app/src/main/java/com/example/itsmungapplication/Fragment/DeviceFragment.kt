7946c61723062bdd5b4fe6af1e8b6646d701e9bb
KimYujin <kyang9494@gmail.com>
2023-05-23 17:41:42 +0900

    initial commit

---
+++ app/src/main/java/com/example/itsmungapplication/Fragment/DeviceFragment.kt
@@ -0,0 +1,21 @@
+package com.example.itsmungapplication.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.itsmungapplication.R
+
+class DeviceFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_device, container, false)
+    }
+
+
+            }
---
+++ app/src/main/java/com/example/itsmungapplication/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.itsmungapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import com.example.itsmungapplication.Fragment.DeviceFragment
+import com.example.itsmungapplication.Fragment.MainFragment
+import com.example.itsmungapplication.Fragment.MypageFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bnv : BottomNavigationView = findViewById(R.id.bnv)
+        val fl : FrameLayout = findViewById(R.id.fl)
+
+        supportFragmentManager.beginTransaction().replace(
+            R.id.fl,
+            MainFragment()
+        ).commit()
+
+        bnv.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.tab1->{ // 기기관리
+                        supportFragmentManager.beginTransaction().replace(
+                            R.id.fl,
+                            DeviceFragment()
+                        ).commit()
+                }
+                R.id.tab2->{ // 메인 페이지
+                        supportFragmentManager.beginTransaction().replace(
+                            R.id.fl,
+                            MainFragment()
+                        ).commit()
+                }
+                R.id.tab3->{ // 마이 페이지
+                        supportFragmentManager.beginTransaction().replace(
+                            R.id.fl,
+                            MypageFragment()
+                        ).commit()
+                }
+
+           }
+                true
+        }
+
+    }
+}
