f6ca90378c4f15bfde11602b0f39aa24b457a9eb
KimYujin <kyang9494@gmail.com>
2023-05-25 17:43:58 +0900

    user page commit

---
+++ app/src/main/java/com/example/itsmungapplication/DoctorListActivity.kt
@@ -0,0 +1,20 @@
+package com.example.itsmungapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ListView
+import androidx.recyclerview.widget.RecyclerView
+
+class DoctorListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doctor_list)
+
+        val lv : ListView = findViewById(R.id.lv)
+
+
+
+
+
+    }
+}
