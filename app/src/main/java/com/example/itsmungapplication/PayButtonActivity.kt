914cc760a4ff7b2ae1f16aa8b54d7b144b592d1c
KimYujin <kyang9494@gmail.com>
2023-05-26 15:18:53 +0900

    middle commit

---
+++ app/src/main/java/com/example/itsmungapplication/PayButtonActivity.kt
@@ -0,0 +1,15 @@
+package com.example.itsmungapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PayButtonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pay_button)
+
+
+
+        
+    }
+}
