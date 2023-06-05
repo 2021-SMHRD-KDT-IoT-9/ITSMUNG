6ac9432085e4526839113766b92c1d401024068a
Park Hyojeong <whalehertz8@gmail.com>
2023-06-05 09:05:58 +0900

    06.03 device wifi

--- app/src/main/java/com/example/itsmungapplication/DeviceActivity.kt
+++ app/src/main/java/com/example/itsmungapplication/DeviceActivity.kt
@@ -3,19 +3,39 @@ package com.example.itsmungapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
 
 class DeviceActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_device)
+//
+//        val btn_device : Button = findViewById(R.id.btn_device)
+//
+//        btn_device.setOnClickListener {
+//            val intent = Intent(this@DeviceActivity,
+//                DevicewifiActivity::class.java)
+//            startActivity(intent)
+//        }
+//    }
+
+    private lateinit var btn_Device: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_device)
 
-        val btn_device : Button = findViewById(R.id.btn_device)
-
-        btn_device.setOnClickListener {
-            val intent = Intent(this@DeviceActivity,
-                DevicewifiActivity::class.java)
-            startActivity(intent)
+        btn_Device = findViewById(R.id.btn_device)
+        btn_Device.setOnClickListener {
+            // Wi-Fi 연결 설정 대화상자 열기
+            openWifiSettingsDialog()
         }
     }
+
+    private fun openWifiSettingsDialog() {
+        val wifiSettingsIntent = Intent(Settings.Panel.ACTION_INTERNET_CONNECTIVITY)
+        startActivity(wifiSettingsIntent)
+    }
+
+
 }
--- app/src/main/java/com/example/itsmungapplication/DevicewifiActivity.kt
+++ app/src/main/java/com/example/itsmungapplication/DevicewifiActivity.kt
@@ -1,64 +1,42 @@
 package com.example.itsmungapplication
 
 
+import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 
 class DevicewifiActivity : AppCompatActivity() {
 
-    private lateinit var wifiManager: WifiManager
-    private lateinit var wifiScanReceiver: BroadcastReceiver
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//    private lateinit var btn_Devicewifi: Button
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_devicewifi)
+//
+//        btn_Devicewifi = findViewById(R.id.btn_devicewifi)
+//        btn_Devicewifi.setOnClickListener {
+//            // Wi-Fi 연결 설정 대화상자 열기
+//            openWifiSettingsDialog()
+//        }
+//    }
+//
+//    private fun openWifiSettingsDialog() {
+//        val wifiSettingsIntent = Intent(Settings.Panel.ACTION_INTERNET_CONNECTIVITY)
+//        startActivity(wifiSettingsIntent)
+//    }
 
-        wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        wifiScanReceiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context, intent: Intent) {
-                val success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
-                if (success) {
-                    scanSuccess()
-                } else {
-                    scanFailure()
-                }
-            }
-        }
-    }
 
-    fun clickWifiScan(view: View) {
-        val success = wifiManager.startScan()
-        if (!success) {
-            Toast.makeText(this, "Wi-Fi Scan에 실패하였습니다.", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun scanSuccess() {
-        val results = wifiManager.scanResults
-        // TODO: Wi-Fi 스캔 결과 사용
-    }
-
-    private fun scanFailure() {
-        // TODO: Wi-Fi 스캔 실패 처리
-        // 이전 스캔 결과 사용 가능: val results = wifiManager.scanResults
-    }
-
-    override fun onResume() {
-        super.onResume()
-        val intentFilter = IntentFilter()
-        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
-        registerReceiver(wifiScanReceiver, intentFilter)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        unregisterReceiver(wifiScanReceiver)
-    }
 }
+
+
