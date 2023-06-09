f27b0a5035f6f8b48ddca6b3fa24d5d27a209ba4
LEE YOUNGJAE <youngjae9683@gmail.com>
2023-06-09 09:09:42 +0900

    0608 화면 구성 변경

--- app/src/main/java/com/example/itsmungapplication/Fragment/MypageFragment.kt
+++ app/src/main/java/com/example/itsmungapplication/Fragment/MypageFragment.kt
@@ -1,6 +1,7 @@
 package com.example.itsmungapplication.Fragment
 
 import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
@@ -20,12 +21,68 @@ import com.example.itsmungapplication.UserInfoActivity
 import com.example.itsmungapplication.UserLoginActivity
 import com.kakao.sdk.user.UserApiClient
 import android.content.ContentValues.TAG
+import android.widget.Toast
+import com.example.itsmungapplication.MainActivity
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+
 class MypageFragment : Fragment() {
 
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var editor: SharedPreferences.Editor
     private lateinit var context: Context
+    // TAG for kakaoLogin - kakaoTalk가 없는 경우
+    private val mCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        // TODO : DB체크를 통해 해당하는 아이디가 있는지 확인합니다.
+        //  없는 경우 UserJoinActivity로 이동합니다.
+        //  있는 경우 MainActivity로 들어갑니다.
+        if (error != null) {
+            Log.e(ContentValues.TAG, "로그인 실패 $error")
+            Toast.makeText(requireContext(), "로그인 실패", Toast.LENGTH_SHORT).show()
+            // 왜 계속 오류가 뜰까?
+            // hash 값 잘못 등록 오ㅗ류 -> 해결
 
+        } else if (token != null) {
+            Log.e(ContentValues.TAG, "로그인 성공 ${token.accessToken}")
+            UserApiClient.instance.me { user, error ->
+                if (error != null) {
+                    Log.e(TAG, "사용자 정보 요청 실패", error)
+                    Toast.makeText(requireContext(), "사용자 정보 요청 실패", Toast.LENGTH_SHORT).show()
+                }
+                else if (user != null) {
+                    var scopes = mutableListOf<String>()
+
+                    if (user.kakaoAccount?.emailNeedsAgreement == true) { scopes.add("account_email") }
+                    //scope 목록을 전달하여 카카오 로그인 요청
+                    UserApiClient.instance.loginWithNewScopes(requireContext(), scopes) { token, error ->
+                        if (error != null) {
+                            Log.e(TAG, "사용자 추가 동의 실패", error)
+                            Toast.makeText(requireContext(), "사용자 추가 동의 실패", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Log.d(TAG, "allowed scopes: ${token!!.scopes}")
+
+                            // 사용자 정보 재요청
+                            UserApiClient.instance.me { user, error ->
+                                if (error != null) {
+                                    Log.e(TAG, "사용자 정보 요청 실패", error)
+                                    Toast.makeText(requireContext(), "사용자 정보 요청 실패", Toast.LENGTH_SHORT).show()
+                                }
+                                else if (user != null) {
+                                    Log.i(TAG, "사용자 정보 요청 성공" +
+                                            "\n회원번호: ${user.id}" +
+                                            "\n이메일: ${user.kakaoAccount?.email}")
+                                    // TODO : 해당 이메일을 DB에 저장 및 등록한다.
+                                }
+                            }
+                        }
+                    }
+
+
+                }
+            }
+        }
+    }
     override fun onAttach(context: Context) {
         super.onAttach(context)
         this.context = context
@@ -47,6 +104,7 @@ class MypageFragment : Fragment() {
         val tv_mypage_userName : TextView = view.findViewById(R.id.tv_mypage_username)
         val tv_mypage_userTel : TextView = view.findViewById(R.id.tv_mypage_usertel)
         val tv_mypage_userAccount : TextView = view.findViewById(R.id.tv_mypage_useraccount)
+        val btn_mypage_kakao : Button = view.findViewById(R.id.btn_mypage_kakao)
 
         sharedPreferences = context.getSharedPreferences("my_app", Context.MODE_PRIVATE)
         editor = sharedPreferences.edit()
@@ -61,7 +119,16 @@ class MypageFragment : Fragment() {
         var tel : String = "010-1234-5678"
         tv_mypage_userTel.setText(tel)
         // 카카오 어떻게 아이디를 보여줄 것인가? 정말 연계된 아이디를 보여줄 것인가?
-        tv_mypage_userAccount.setText("")
+        // DB 카카오 로그인 연계가 되어있는 경우
+        val kakaoLoginCheck : Boolean = true
+        if(!kakaoLoginCheck){
+            tv_mypage_userAccount.setText("")
+            btn_mypage_kakao.isEnabled = true
+        }
+        else{
+            tv_mypage_userAccount.setText("카카오 연결 완료")
+            btn_mypage_kakao.isEnabled = false
+        }
 
 
 
@@ -104,7 +171,70 @@ class MypageFragment : Fragment() {
             val intent = Intent(activity,NoticeActivity::class.java)
             startActivity(intent)
         }
+        btn_mypage_kakao.setOnClickListener {
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(requireContext())) {
+                // 카카오톡 로그인
+                UserApiClient.instance.loginWithKakaoTalk(requireContext()) { token, error ->
+                    // 로그인 실패 부분
+                    if (error != null) {
+                        Log.e(TAG, "로그인 실패 $error")
+                        Log.d("TAG", TAG)
+                        Toast.makeText(requireContext(), "로그인 실패", Toast.LENGTH_SHORT).show()
+                        // 사용자가 취소
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            return@loginWithKakaoTalk
+                        }
+                        // 다른 오류
+                        else {
+                            UserApiClient.instance.loginWithKakaoAccount(
+                                requireContext(),
+                                callback = mCallback
+                            ) // 카카오 이메일 로그인
+                        }
+                    }
+                    // 로그인 성공 부분
+                    else if (token != null) {
+                        Log.e(ContentValues.TAG, "로그인 성공 ${token.accessToken}")
+                        UserApiClient.instance.me { user, error ->
+                            if (error != null) {
+                                Log.e(TAG, "사용자 정보 요청 실패", error)
+                                Toast.makeText(requireContext(), "사용자 정보 요청 실패", Toast.LENGTH_SHORT).show()
+                            }
+                            else if (user != null) {
+                                var scopes = mutableListOf<String>()
 
+                                if (user.kakaoAccount?.emailNeedsAgreement == true) { scopes.add("account_email") }
+                                //scope 목록을 전달하여 카카오 로그인 요청
+                                UserApiClient.instance.loginWithNewScopes(requireContext(), scopes) { token, error ->
+                                    if (error != null) {
+                                        Log.e(TAG, "사용자 추가 동의 실패", error)
+                                        Toast.makeText(requireContext(), "사용자 추가 동의 실패", Toast.LENGTH_SHORT).show()
+                                    } else {
+                                        Log.d(TAG, "allowed scopes: ${token!!.scopes}")
+
+                                        // 사용자 정보 재요청
+                                        UserApiClient.instance.me { user, error ->
+                                            if (error != null) {
+                                                Log.e(TAG, "사용자 정보 요청 실패", error)
+                                                Toast.makeText(requireContext(), "사용자 정보 요청 실패", Toast.LENGTH_SHORT).show()
+                                            }
+                                            else if (user != null) {
+                                                Log.i(TAG, "사용자 정보 요청 성공" +
+                                                        "\n회원번호: ${user.id}" +
+                                                        "\n이메일: ${user.kakaoAccount?.email}")
+                                                // TODO : 해당 이메일을 DB에 저장 및 등록한다.
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = mCallback) // 카카오 이메일 로그인
+            }
+        }
 
         return view
     }
